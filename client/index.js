2bf9464416d4f7f37e1eca61a00347167bd33f41
Tomasz Tendera <tomektendera@gmail.com>
2022-04-21 14:39:01 +0200

    Update main ping animation

--- client/index.js
+++ client/index.js
@@ -1,12 +1,14 @@
 class ImageInstance {
-    constructor(pos_x, pos_y, id_name){
+    constructor(pos_x, pos_y, id_name, delay=0){
         this.id = id_name;
         this.posx = pos_x;
         this.posy = pos_y;
         this.element = null;
         this.image = null;
-        this.style = null;
+        this.element_style = null;
+        this.image_style = null;
         this.id_name = 'id'+this.id;
+        this.delay = delay;
         
     }
     createOnSite(){
@@ -16,13 +18,14 @@ class ImageInstance {
             this.element.setAttribute("id", this.id_name);
             this.image = document.createElement('img');
             this.image.setAttribute('src', './usb_desc_backup.png');
+            this.image.setAttribute('id', 'img'+this.id);
             // this.image.setAttribute("id", this.id_name);
             this.element.appendChild(this.image);
         }
         document.getElementById("main").appendChild(this.element);
         // document.body.appendChild(this.element);
         this.styleElement();
-        document.getElementById("main").appendChild(this.style);
+        document.getElementById("main").appendChild(this.element_style);
         // document.body.appendChild(this.style);
     }
 
@@ -31,14 +34,15 @@ class ImageInstance {
         var xpos = (this.posx - on_site.offsetLeft - 128/2);
         var ypos = (this.posy - on_site.offsetTop - 128/2);
         
-        this.style = document.createElement('style');
-        this.style.setAttribute("id", this.id_name);
-        this.style.innerHTML = '#id' + this.id + ' {position: fixed; width: 128px; height: 128px; transform: translate(' + xpos + 'px, ' + ypos + 'px);';
+        this.element_style = document.createElement('style');
+        this.element_style.setAttribute("id", this.id_name);
+        this.element_style.innerHTML = `#id${this.id} {visibility: hidden; position: fixed; width: 128px; height: 128px; transform: translate(${xpos}px, ${ypos}px);}`;
+        this.element_style.innerHTML += `#img${this.id}{animation-name: anima; animation-duration: 1.5s; animation-fill-mode: forwards; animation-delay: ${this.delay}ms;}`
     }
 
     removeThis(){
         this.element.remove();
-        this.style.remove();
+        this.element_style.remove();
     }
 }
 
@@ -53,7 +57,8 @@ var confetti;
 window.onload = function()
 {
     document.addEventListener('click', printMousePos);
-    confetti = new JSConfetti();
+    const canvas = document.getElementById('main_canvas');
+    confetti = new JSConfetti({canvas});
 }
 
 function createInClick(X, Y) {
@@ -92,14 +97,16 @@ function getRandom(min, max) {
 function showRandomPings(number){
     var x = 0;
     var y = 0;
+    var delay = 0;
     var new_element;
     for (let index = 0; index < number; index++) {
         var x = getRandom(0, 1820);
-        var y = getRandom(0, 900);
-        new_element = new ImageInstance(x, y, index);
+        var y = getRandom(0, 1080);
+        var delay = getRandom(0, 2000);
+        new_element = new ImageInstance(x, y, index, delay);
         new_element.createOnSite();
         elements.push(new_element);
-        setTimeout(removeOneElem, 1500);
+        setTimeout(removeOneElem, 1500 + delay);
     }
 }
 
